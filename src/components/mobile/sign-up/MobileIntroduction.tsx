73bd2d4abe66e1f690f79e9654ce866abb3cc2f5
ergi98 <edervishaj98@gmail.com>
2022-03-22 20:57:17 +0100

    Split signup personal info into reusable components

--- src/components/mobile/sign-up/MobileIntroduction.tsx
+++ src/components/mobile/sign-up/MobileIntroduction.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+// Formik
+import { Card, CardContent, Paper } from "@mui/material";
 
-function MobileIntroduction() {
-  return (
-    <div>MobileIntroduction</div>
-  )
+// Formik
+import { useFormikContext } from "formik";
+
+// Components
+import IntroductionText from "../../shared/sign-up/IntroductionText";
+import IntroductionFields from "../../shared/sign-up/IntroductionFields";
+
+interface PropsInterface {
+  saveContext: (a: string, b: any) => void;
 }
 
-export default MobileIntroduction
+function MobileIntroduction(props: PropsInterface) {
+  const formik: any = useFormikContext();
+
+  return (
+    <>
+      <IntroductionText
+        className="text-white px-3"
+        name={formik.values.name}
+        surname={formik.values.surname}
+      />
+      <Card
+        className="w-full rounded-t-3xl p-3 pt-8 pb-env h-fit"
+        sx={{
+          borderTopLeftRadius: "1.5rem",
+          borderTopRightRadius: "1.5rem",
+        }}
+      >
+        <CardContent children={<IntroductionFields />} />
+      </Card>
+    </>
+  );
+}
+
+export default MobileIntroduction;
