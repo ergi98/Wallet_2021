f40b35f11ec79b6de01eac4c33968efa57b6cd3a
ergi98 <edervishaj98@gmail.com>
2022-03-22 21:05:12 +0100

    GUI of mobile personal info

--- src/components/mobile/sign-up/MobilePersonalInfo.tsx
+++ src/components/mobile/sign-up/MobilePersonalInfo.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+// Formik
+import { useFormikContext } from "formik";
+
+// MUI
+import { Card, CardContent } from "@mui/material";
+
+// Components
+import PersonalInfoText from "../../shared/sign-up/PersonalInfoText";
+import PersonalInfoFields from "../../shared/sign-up/PersonalInfoFields";
 
 function MobilePersonalInfo() {
-  return <div>MobilePersonalInfo</div>;
+  const formik: any = useFormikContext();
+
+  return (
+    <>
+      <PersonalInfoText className="text-white px-3" name={formik.values.name} />
+      <Card
+        className="w-full rounded-t-3xl p-3 pt-8 pb-env h-fit"
+        sx={{
+          borderTopLeftRadius: "1.5rem",
+          borderTopRightRadius: "1.5rem",
+        }}
+      >
+        <CardContent children={<PersonalInfoFields />} />
+      </Card>
+    </>
+  );
 }
 
 export default MobilePersonalInfo;
--- src/components/shared/sign-up/PersonalInfoText.tsx
+++ src/components/shared/sign-up/PersonalInfoText.tsx
@@ -3,14 +3,17 @@ import { Typography } from "@mui/material";
 
 interface PropsInterface {
   name: string;
+  className?: string;
 }
 
 function PersonalInfoText(props: PropsInterface) {
   return (
     <>
-      <Typography variant="subtitle1">Alright {props.name},</Typography>
+      <Typography className={props.className} variant="subtitle1">
+        Alright {props.name},
+      </Typography>
       <Typography
-        className=" w-11/12 pb-3 whitespace-nowrap overflow-hidden text-ellipsis"
+        className={`w-11/12 pb-3 whitespace-nowrap overflow-hidden text-ellipsis ${props.className}`}
         variant="h4"
         gutterBottom
       >
