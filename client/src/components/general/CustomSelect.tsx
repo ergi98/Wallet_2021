944e3a7af92fc647ef429300e97cdadc71047e87
ergi98 <edervishaj98@gmail.com>
2022-04-03 11:50:48 +0200

    Mobile select

--- client/src/components/general/CustomSelect.tsx
+++ client/src/components/general/CustomSelect.tsx
@@ -1,63 +1,102 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Formik
 import { useFormikContext } from "formik";
 
 // MUI
-import {
-  FormHelperText,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
+import { MenuItem, TextField } from "@mui/material";
+
+// Utilities
+import { isMobile } from "../../utilities/mobile-utilities";
+
+// Components
+import BottomDialog from "./BottomDialog";
+import MobileSelect from "../mobile/select/MobileSelect";
 
 interface SelectOption {
-  text: String;
-  value: String;
+	text: string;
+	value: string;
 }
 
 interface PropsInterface {
-  options?: Array<SelectOption>;
-  fieldName: string;
-  label: string;
-  required?: boolean;
+	label: string;
+	fieldName: string;
+	required?: boolean;
+	options?: Array<SelectOption>;
 }
 
 function CustomSelect({ fieldName, label, options, required }: PropsInterface) {
-  const [selectOptions, setSelectOptions] = useState<Array<any>>(
-    () => options || []
-  );
+	const formik: any = useFormikContext();
 
-  const formik: any = useFormikContext();
+	const [selectOptions] = useState<Array<any>>(() => options || []);
+	const [showSelect, setShowSelect] = useState<boolean>(false);
 
-  return (
-    <TextField
-      label={label}
-      name={fieldName}
-      value={formik.values[fieldName]}
-      onChange={formik.handleChange}
-      onBlur={formik.handleBlur}
-      error={!!formik.errors[fieldName] && formik.touched[fieldName]}
-      helperText={
-        !!formik.errors[fieldName] && formik.touched[fieldName]
-          ? formik.errors[fieldName]
-          : " "
-      }
-      required={required}
-      autoComplete="off"
-      size="small"
-      fullWidth
-      select
-    >
-      {selectOptions.map((option) => (
-        <MenuItem key={option.value} value={option.value}>
-          {option.text}
-        </MenuItem>
-      ))}
-    </TextField>
-  );
+	const toggleShowSelect = (value: boolean) => setShowSelect(value);
+
+	const handleFocus = () => isMobile && setShowSelect(true);
+
+	useEffect(() => {
+		function removeFocusOnClose(show: boolean) {
+			if (!show) {
+				let activeElement = document.activeElement as HTMLElement;
+				activeElement.blur();
+			}
+		}
+		removeFocusOnClose(showSelect);
+	}, [showSelect]);
+
+	function handleMobileChange(value: Array<string> | string) {
+		formik.setFieldValue(fieldName, value);
+		setShowSelect(false);
+	}
+
+	return (
+		<>
+			<TextField
+				label={label}
+				name={fieldName}
+				onClick={handleFocus}
+				onBlur={formik.handleBlur}
+				onChange={formik.handleChange}
+				value={formik.values[fieldName]}
+				error={!!formik.errors[fieldName] && formik.touched[fieldName]}
+				helperText={
+					!!formik.errors[fieldName] && formik.touched[fieldName]
+						? formik.errors[fieldName]
+						: " "
+				}
+				SelectProps={{
+					readOnly: isMobile,
+				}}
+				required={required}
+				autoComplete="off"
+				size="small"
+				fullWidth
+				select
+			>
+				{selectOptions.map((option) => (
+					<MenuItem key={option.value} value={option.value}>
+						{option.text}
+					</MenuItem>
+				))}
+			</TextField>
+			{showSelect && isMobile && (
+				<BottomDialog
+					open={showSelect}
+					onClose={toggleShowSelect}
+					closeOnSwipe={true}
+				>
+					<MobileSelect
+						search={true}
+						options={selectOptions}
+						label={"Select Gender"}
+						value={formik.values[fieldName]}
+						onChange={handleMobileChange}
+					/>
+				</BottomDialog>
+			)}
+		</>
+	);
 }
 
 export default CustomSelect;
