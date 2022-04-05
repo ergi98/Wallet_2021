a9e739f1a1f4229c7e36c96b8624661c32e24f84
ergi98 <edervishaj98@gmail.com>
2022-04-05 13:11:40 +0200

    Auth api frontend calls file

---
+++ client/src/api/auth-api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+async function logIn() {
+	try {
+	} catch (err) {}
+}
+
+async function logOut() {
+	try {
+	} catch (err) {}
+}
+
+// Validates a given username if it is free or not
+async function validate() {
+	try {
+	} catch (err) {}
+}
+
+async function signUp() {
+	try {
+	} catch (err) {}
+}
+
+export { logIn, logOut, validate, signUp };
