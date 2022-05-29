10ad8abd3cc03358a323851d3be573b0095869f1
ergi98 <edervishaj98@gmail.com>
2022-05-29 14:58:34 +0200

    Multiple fallbacks to currency rates fetch

--- controllers/currency-rates-controller.js
+++ controllers/currency-rates-controller.js
@@ -10,14 +10,58 @@ import BigNumber from "bignumber.js";
 import CurrencySchema from "../schemas/currency-schema.js";
 import CurrencyRatesSchema from "../schemas/currency-rates-schema.js";
 
+// Fetch
+import fetch, { Headers } from "node-fetch";
+
+// Last fallback
+const hardcodedRates = {
+	ALL: 1,
+	USD: 112.8,
+	EUR: 120.94,
+	GBP: 142.15,
+	AUD: 80.56,
+	CAD: 88.56,
+	CHF: 117.63,
+};
+
+const compareIds = (id1, id2) =>
+	mongoose.Types.ObjectId(id1).equals(mongoose.Types.ObjectId(id2));
+
 BigNumber.set({ DECIMAL_PLACES: 4, ROUNDING_MODE: 4 });
+
+function getRatesFromLatestStored(currencies, latestRates) {
+	const rates = [];
+	const ALL_CURRENCY = currencies.find(
+		(currency) => currency.acronym === "ALL"
+	);
+	// For each currency
+	for (const currency of currencies) {
+		if (currency.acronym === "ALL") continue;
+		// Find the latest rate to ALL
+		const latestCurrencyRates = latestRates.find((doc) =>
+			compareIds(doc.currency, currency._id)
+		);
+		let rate;
+		if (latestCurrencyRates) {
+			const rateToALL = latestCurrencyRates.rates.find((rate) =>
+				compareIds(rate._id, ALL_CURRENCY._id)
+			);
+			rate = rateToALL?.rate ?? hardcodedRates[currency.acronym];
+		}
+		// If for whatever reason we do not have any entry fallback to hardcoded values
+		else rate = hardcodedRates[currency.acronym];
+		rates.push({
+			acronym: currency.acronym,
+			rateToALL: new BigNumber(rate).toNumber(),
+		});
+	}
+	return rates;
+}
 
 async function fetchRatesWithPuppeteer() {
 	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
-	await page.goto(
-		"https://www.bankofalbania.org/Tregjet/Kursi_zyrtar_i_kembimit/"
-	);
+	await page.goto(process.env.RATES_URL);
 	await page.waitForSelector(".table");
 
 	const rates = await page.evaluate(() => {
@@ -59,29 +103,91 @@ async function fetchRatesWithPuppeteer() {
 			throw new Error("Could not extract currency data");
 		return results;
 	});
+	return rates;
+}
 
+async function fetchRatesWithExchangeAPI(currencies) {
+	const requestHeaders = new Headers();
+	requestHeaders.append("apikey", process.env.EXCHANGE_API_KEY);
+	const requestOptions = {
+		method: "GET",
+		redirect: "follow",
+		headers: requestHeaders,
+	};
+	const base = "ALL";
+	const symbols = currencies
+		.map((currency) => currency.acronym)
+		.filter((currency) => currency !== "ALL");
+	const response = await fetch(
+		`https://api.apilayer.com/exchangerates_data/latest?symbols=${symbols}&base=${base}`,
+		requestOptions
+	);
+	const data = await response.json();
+
+	const rates = [];
+
+	const one = new BigNumber("1");
+
+	// Transforming in the same format that puppeteer returns data;
+	for (const key of Object.keys(data.rates)) {
+		const rate = new BigNumber(data.rates[key].toString());
+		const obj = {
+			acronym: key,
+			rateForALL: one.dividedBy(rate).toNumber(),
+		};
+		rates.push(obj);
+	}
 	return rates;
 }
 
 async function populateRatesFromBOA(req, res) {
 	try {
-		const currencies = await CurrencySchema.find({});
+		const createdAt = new Date();
 
-		let availableCurrencies = {};
+		const startOfDay = new Date(createdAt.setUTCHours(0, 0, 0, 0));
+		const endOfDay = new Date(createdAt.setUTCHours(23, 59, 59, 999));
 
-		for (let currency of currencies) {
+		const [currencies, latestStoredRates] = await Promise.all([
+			CurrencySchema.find({}),
+			CurrencyRatesSchema.find({
+				createdAt: {
+					$gte: startOfDay,
+					$lte: endOfDay,
+				},
+			}),
+		]);
+
+		const availableCurrencies = {};
+
+		// Creating a acronym -> id object
+		for (const currency of currencies) {
 			availableCurrencies[currency.acronym] = currency._id;
 		}
 
-		const fetchedRates = await fetchRatesWithPuppeteer();
+		let fetchedRates;
+
+		// Try to get rates with puppeteer
+		try {
+			fetchedRates = await fetchRatesWithPuppeteer();
+		} catch (err) {
+			// If that fails try to fetch rates with exchange api
+			try {
+				fetchedRates = await fetchRatesWithExchangeAPI(currencies);
+			} catch (err) {
+				// If that fails fallback to latest successfully fetched rates (or at the very end hardcoded values)
+				fetchedRates = getRatesFromLatestStored(currencies, latestStoredRates);
+			}
+		}
 
 		if (!Array.isArray(fetchedRates) || fetchedRates?.length === 0)
 			throw new Error("Could not extract currency rates.");
 
+		// Filtering the rates to only the ones we are interested in
 		const filteredRates = fetchedRates.filter(
 			(rate) => availableCurrencies[rate.acronym] !== undefined
 		);
 
+		// If there are missing data or no data at all throw
 		if (!Array.isArray(filteredRates) || filteredRates?.length === 0)
 			throw new Error(
 				"Currency data does not include any currency we store in DB."
@@ -89,14 +195,15 @@ async function populateRatesFromBOA(req, res) {
 		else if (filteredRates.length !== currencies.length - 1)
 			throw new Error("Could not fetch data for all the needed currencies");
 
+		// ALL -> ALL is always 1
 		filteredRates.push({
 			acronym: "ALL",
 			rateForALL: 1,
 		});
 
 		// Calculating rates for each currency to each currency
-		for (let fromEntry of filteredRates) {
-			for (let toEntry of filteredRates) {
+		for (const fromEntry of filteredRates) {
+			for (const toEntry of filteredRates) {
 				const fromRateToALL = new BigNumber(fromEntry.rateForALL);
 				const toRateToALL = new BigNumber(toEntry.rateForALL);
 				if (fromEntry.rates === undefined) {
@@ -119,18 +226,6 @@ async function populateRatesFromBOA(req, res) {
 			}
 		}
 
-		let createdAt = new Date();
-
-		let startOfDay = new Date(createdAt.setUTCHours(0, 0, 0, 0));
-		let endOfDay = new Date(createdAt.setUTCHours(23, 59, 59, 999));
-
-		let latestStoredRates = await CurrencyRatesSchema.find({
-			createdAt: {
-				$gte: startOfDay,
-				$lte: endOfDay,
-			},
-		});
-
 		// First time inserting these rates for today
 		if (latestStoredRates.length === 0) {
 			const currencyRatesArray = [];
@@ -149,7 +244,7 @@ async function populateRatesFromBOA(req, res) {
 		// Called later on the same day - Just update
 		else {
 			const bulkUpdateArray = [];
-			for (let rate of filteredRates) {
+			for (const rate of filteredRates) {
 				let currencyId = availableCurrencies[rate.acronym];
 				if (!currencyId)
 					throw new Error("Something went wrong when fetching currency rates");
@@ -162,7 +257,6 @@ async function populateRatesFromBOA(req, res) {
 			}
 			await CurrencyRatesSchema.bulkWrite(bulkUpdateArray);
 		}
-
 		res.status(200).send({ success: true });
 	} catch (err) {
 		console.error(err);
